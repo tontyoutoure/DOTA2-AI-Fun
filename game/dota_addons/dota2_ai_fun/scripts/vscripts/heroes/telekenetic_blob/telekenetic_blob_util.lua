fcade08d1ba48836063f6f622ef33bdf2943fc16
yanmofeixi <yanmofeixi@gmail.com>
2017-07-24 17:59:21 -0700

    Fix throwing

--- game/dota_addons/dota2_ai_fun/scripts/vscripts/heroes/telekenetic_blob/telekenetic_blob_util.lua
+++ game/dota_addons/dota2_ai_fun/scripts/vscripts/heroes/telekenetic_blob/telekenetic_blob_util.lua
@@ -14,15 +14,17 @@ function TelekeneticBlobFlySetup(modifier, fixedSpeed)
 
     modifier.vStartPosition    = GetGroundPosition( modifier:GetParent():GetOrigin(), modifier:GetParent() )
     modifier.vTargetPosition   = modifier:GetAbility():GetCursorPosition()
+	modifier.flStartHeight = GetGroundHeight( modifier.vStartPosition, modifier:GetParent())
+	modifier.flTargetHeight = GetGroundHeight( modifier.vTargetPosition, modifier:GetParent())
+	print(modifier.flStartHeight)
+	print(modifier.flTargetHeight)
     modifier.flHeight          = modifier:GetAbility():GetSpecialValueFor("fly_height")
     modifier.vDirection        = (modifier.vTargetPosition - modifier.vStartPosition):Normalized()
     modifier.flDistance        = (modifier.vTargetPosition - modifier.vStartPosition):Length2D()
 	if fixedSpeed then
 		modifier.flHorizontalSpeed = modifier:GetAbility():GetSpecialValueFor("fly_speed")
-		modifier.flDuration = modifier.flDistance / modifier.flHorizontalSpeed
 	else
-		modifier.flDuration        = modifier:GetAbility():GetSpecialValueFor("fly_duration")
-		modifier.flHorizontalSpeed = modifier.flDistance / modifier.flDuration
+		modifier.flHorizontalSpeed = modifier.flDistance / modifier:GetAbility():GetSpecialValueFor("fly_duration")
 	end
     EmitSoundOnLocationWithCaster(modifier.vStartPosition, "Ability.TossThrow", modifier:GetParent())
 end
@@ -38,7 +40,6 @@ function TelekeneticBlobFlyUpdateHorizontal(me, dt, modifier)
 	if IsServer() then
         local vOldPosition = me:GetOrigin()
         local vNewPos      = vOldPosition + modifier.vDirection * modifier.flHorizontalSpeed * dt
-        vNewPos.z          = 0
         me:SetOrigin(vNewPos)
     end
 end
@@ -47,12 +48,12 @@ function TelekeneticBlobFlyUpdateVertical(me, dt, modifier, landingCallback)
     if IsServer() then
         local vOrigin        = me:GetOrigin()
         local vDistance      = (vOrigin - modifier.vStartPosition):Length2D()
-        local vZ             = - 4 * modifier.flHeight / (modifier.flDistance * modifier.flDistance) * (vDistance * vDistance) + 4 * modifier.flHeight / modifier.flDistance * vDistance
+        local vZ             = (2*modifier.flTargetHeight-2*modifier.flStartHeight-4*modifier.flHeight)/(modifier.flDistance*modifier.flDistance) * (vDistance*vDistance) + (4*modifier.flHeight+modifier.flStartHeight-modifier.flTargetHeight) / modifier.flDistance * vDistance + modifier.flStartHeight
         vOrigin.z            = vZ
         local flGroundHeight = GetGroundHeight( vOrigin, modifier:GetParent() )
         local bLanded        = false
 
-        if ( vOrigin.z < flGroundHeight and vDistance > modifier.flDistance / 2 ) then
+        if (vDistance >= modifier.flDistance) then
             vOrigin.z = flGroundHeight
             bLanded   = true
         end
