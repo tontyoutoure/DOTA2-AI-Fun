d98db963d4c8e506bca5c2fa2efec4a02bbf624c
aaaazzzzpo <77374911+aaaazzzzpo@users.noreply.github.com>
2021-01-16 20:28:07 +0800

    更新BOT出装，新添加了BOT合成智力跳刀的公式
    更新BOT出装，新添加了BOT合成智力跳刀的公式

---
+++ bot_item_data.lua
@@ -0,0 +1,218 @@
+tBotItemData = {}
+tBotItemData.tFunItems = {
+	item_fun_economizer = {"item_octarine_core","item_mystic_staff","item_staff_of_wizardry","item_robe","item_mantle"},
+	item_fun_economizer_2 = {"item_fun_economizer","item_ultimate_scepter","item_kaya"},
+	item_fun_genji_gloves = {"item_slippers","item_boots_of_elves","item_blade_of_alacrity","item_eagle","item_butterfly"},
+	item_fun_genji_gloves_2 = {"item_fun_genji_gloves","item_monkey_king_bar","item_yasha"},
+	item_fun_ragnarok = {"item_gauntlets","item_belt_of_strength","item_ogre_axe","item_reaver","item_heart"},
+	item_fun_ragnarok_2 = {"item_fun_ragnarok", "item_bfury", "item_sange"},
+	item_fun_escutcheon = {"item_pipe","item_crimson_guard","item_aeon_disk","item_soul_booster"},
+	item_fun_angelic_alliance = {"item_fun_sprint_shoes","item_fun_economizer_2","item_rapier"},
+	item_fun_blood_sword = {"item_greater_crit","item_satanic","item_echo_sabre","item_echo_sabre"},
+	item_fun_heros_bow = {"item_ethereal_blade","item_hurricane_pike","item_nullifier"},
+	item_fun_magic_hammer = {"item_rod_of_atos","item_shivas_guard","item_diffusal_blade","item_aether_lens"},
+	item_fun_terra_blade = {"item_fun_ragnarok_2","item_fun_genji_gloves_2"},	
+	item_fun_orb_of_omnipotence = {"item_ultimate_orb","item_reaver","item_eagle","item_mystic_staff"},
+	item_fun_sprint_shoes = {"item_blink","item_travel_boots_2","item_wind_lace"},
+	item_fun_papyrus_scarab = {"item_assault", "item_guardian_greaves", "item_vladmir", "item_helm_of_the_dominator", "item_hand_of_midas"},
+	item_fun_bs = {"item_dagon_5", "item_bloodstone", "item_holy_locket"},
+}
+
+tBotItemData.tItemUpgrades = {
+	item_fun_economizer = {"item_fun_economizer_2"},
+	item_fun_genji_gloves = {"item_fun_genji_gloves_2", "item_fun_terra_blade"},
+	item_fun_ragnarok = {"item_fun_ragnarok_2", "item_fun_terra_blade"},
+	item_fun_genji_gloves_2 = {"item_fun_terra_blade"},
+	item_fun_ragnarok_2 = {"item_fun_terra_blade"},
+}
+
+tBotItemData.tNotUpgrade = {
+	item_aegis = true,
+	item_courier = true,
+	item_boots_of_elves = true,
+	item_belt_of_strength = true,
+	item_blade_of_alacrity = true,
+	item_blades_of_attack = true,
+	item_blight_stone = true,
+	item_blink = true,
+	item_boots = true,
+	item_bottle = true,
+	item_broadsword = true,
+	item_chainmail = true,
+	item_cheese = true,
+	item_circlet = true,
+	item_clarity = true,
+	item_claymore = true,
+	item_cloak = true,
+	item_demon_edge = true,
+	item_dust = true,
+	item_eagle = true,
+	item_enchanted_mango = true,
+	item_energy_booster = true,
+	item_faerie_fire = true,
+	item_flying_courier = true,
+	item_gauntlets = true,
+	item_gem = true,
+	item_ghost = true,
+	item_gloves = true,
+	item_flask = true,
+	item_helm_of_iron_will = true,
+	item_hyperstone = true,
+	item_infused_raindrop = true,
+	item_branches = true,
+	item_javelin = true,
+	item_magic_stick = true,
+	item_mantle = true,
+	item_mithril_hammer = true,
+	item_lifesteal = true,
+	item_mystic_staff = true,
+	item_ward_observer = true,
+	item_ogre_axe = true,
+	item_orb_of_venom = true,
+	item_platemail = true,
+	item_point_booster = true,
+	item_quarterstaff = true,
+	item_quelling_blade = true,
+	item_reaver = true,
+	item_ring_of_health = true,
+	item_ring_of_protection = true,
+	item_ring_of_regen = true,
+	item_robe = true,
+	item_relic = true,
+	item_sobi_mask = true,
+	item_ward_sentry = true,
+	item_shadow_amulet = true,
+	item_slippers = true,
+	item_smoke_of_deceit = true,
+	item_staff_of_wizardry = true,
+	item_stout_shield = true,
+	item_talisman_of_evasion = true,
+	item_tango = true,
+	item_tango_single = true,
+	item_tome_of_knowledge = true,
+	item_tpscroll = true,
+	item_ultimate_orb = true,
+	item_vitality_booster = true,
+	item_void_stone = true,
+	item_wind_lace = true
+}
+
+tBotItemData.tItemParts = {
+	item_solar_crest = {'item_medallion_of_courage'},
+	item_mjollnir = {'item_maelstrom'},
+	item_guardian_greaves = {'item_mekansm', 'item_arcane_boots'},
+	item_hurricane_pike = {'item_dragon_lance', 'item_force_staff'},
+	item_ultimate_scepter_2 = {'item_ultimate_scepter'},
+	item_abyssal_blade = {'item_basher','item_vanguard'},
+                item_arcane_blink = {'item_blink','item_mystic_staff'},
+}
+
+tBotItemData.tLuxuryItemList = {
+	npc_dota_hero_axe = {'item_shivas_guard', 'item_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_bane = {'item_necronomicon_3', 'item_octarine_core', 'item_shivas_guard', 'item_sphere', 'item_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_bloodseeker = {'item_vanguard', 'item_abyssal_blade', 'item_radiance','item_butterfly', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_bounty_hunter = {'item_black_king_bar', 'item_assault', 'item_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_crystal_maiden = {'item_black_king_bar', 'item_shivas_guard', 'item_sheepstick', 'item_octarine_core', 'item_monkey_king_bar', 'item_recipe_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_drow_ranger = {"item_satanic", "item_mjollnir", "item_greater_crit", "item_ultimate_scepter_2", 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_dazzle = {'item_orchid', 'item_guardian_greaves', 'item_octarine_core', 'item_shivas_guard', 'item_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_earthshaker = {'item_sheepstick', 'item_greater_crit', 'item_recipe_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_juggernaut = {'item_satanic', 'item_monkey_king_bar', 'item_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_pudge = {'item_ultimate_scepter', 'item_octarine_core', 'item_recipe_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_nevermore = {'item_butterfly', 'item_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_death_prophet = {'item_octarine_core', 'item_shivas_guard', 'item_sheepstick', 'item_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_sniper = {'item_maelstrom','item_hurricane_pike', 'item_manta', 'item_black_king_bar', 'item_mjollnir', 'item_greater_crit', 'item_aghanims_shard', 'item_arcane_blink'}, --ulti ok?
+	npc_dota_hero_jakiro = {'item_ultimate_scepter', 'item_guardian_greaves', 'item_shivas_guard', 'item_octarine_core', 'item_refresher', 'item_sphere', 'item_recipe_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_kunkka = {'item_assault', 'item_bfury', 'item_monkey_king_bar', 'item_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_lina = {'item_ultimate_scepter', "item_cyclone", "item_octarine_core", "item_hurricane_pike", 'item_monkey_king_bar', 'item_recipe_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_lion = {"item_dagon_5", 'item_octarine_core', 'item_recipe_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_luna = {"item_assault", "item_satanic", "item_ultimate_scepter_2", 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_omniknight = {"item_heart", "item_guardian_greaves", "item_ultimate_scepter_2", 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_oracle = {"item_orchid","item_guardian_greaves", 'item_octarine_core', "item_aeon_disk", "item_ultimate_scepter_2", 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_sand_king = {"item_shivas_guard", "item_sphere", "item_ultimate_scepter_2", 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_sven = {"item_satanic", "item_greater_crit", 'item_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_tiny = {"item_heart", "item_assault", 'item_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_warlock = {'item_octarine_core','item_sphere', 'item_shivas_guard','item_recipe_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_vengefulspirit = {"item_aether_lens", "item_hurricane_pike", "item_butterfly", "item_assault", 'item_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_viper = {"item_monkey_king_bar", 'item_butterfly', "item_guardian_greaves", 'item_recipe_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_witch_doctor = {"item_black_king_bar", "item_guardian_greaves", 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_zuus = {'item_bloodstone', 'item_aether_lens', 'item_refresher', 'item_sheepstick', 'item_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_windrunner = {"item_mjollnir", "item_hurricane_pike", 'item_greater_crit', 'item_recipe_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_lich = {'item_ultimate_scepter','item_octarine_core','item_recipe_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_necrolyte = {'item_octarine_core', 'item_sphere', 'item_recipe_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_skeleton_king = {'item_assault', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_phantom_assassin = {'item_vanguard', 'item_abyssal_blade', 'item_satanic', 'item_assault' , 'item_monkey_king_bar','item_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_dragon_knight = {'item_assault', 'item_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_chaos_knight = {'item_assault', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_skywrath_mage = {'item_sheepstick', 'item_octarine_core', 'item_recipe_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'},
+	npc_dota_hero_bristleback = {'item_vanguard', 'item_blade_mail', 'item_heart', 'item_assault', 'item_shivas_guard', 'item_radiance', 'item_ultimate_scepter_2', 'item_aghanims_shard', 'item_arcane_blink'}, -- ability ok?
+}
+
+tBotItemData.tBotBuildFunItems = {
+	npc_dota_hero_axe = {tWantedFunItems = {"item_fun_ragnarok","item_fun_ragnarok_2","item_fun_magic_hammer","item_fun_escutcheon"}, sEndItem = "item_shivas_guard"},
+	npc_dota_hero_bane = {tWantedFunItems = {"item_fun_magic_hammer","item_fun_economizer","item_fun_economizer_2","item_fun_escutcheon"}, sEndItem = "item_shivas_guard"},
+	npc_dota_hero_bounty_hunter = {tWantedFunItems = {"item_fun_papyrus_scarab","item_fun_heros_bow","item_fun_economizer","item_fun_economizer_2"}, sEndItem = "item_assault"},
+	npc_dota_hero_bloodseeker = {tWantedFunItems = {"item_fun_genji_gloves","item_fun_genji_gloves_2","item_fun_ragnarok","item_fun_ragnarok_2","item_fun_blood_sword","item_fun_escutcheon"}, sEndItem = "item_butterfly"},
+	npc_dota_hero_crystal_maiden = {tWantedFunItems = {"item_fun_magic_hammer","item_fun_economizer","item_fun_economizer_2","item_fun_escutcheon"}, sEndItem = "item_black_king_bar"},
+	npc_dota_hero_drow_ranger = {tWantedFunItems = {"item_fun_genji_gloves","item_fun_genji_gloves_2","item_fun_blood_sword","item_fun_heros_bow"}, sEndItem = "item_greater_crit"},
+	npc_dota_hero_dazzle = {tWantedFunItems = {"item_fun_papyrus_scarab", "item_fun_economizer","item_fun_economizer_2","item_fun_magic_hammer"}, sEndItem = "item_orchid"},
+	npc_dota_hero_earthshaker = {tWantedFunItems = {"item_fun_magic_hammer","item_fun_economizer","item_fun_economizer_2","item_fun_escutcheon"}, sEndItem = "item_ultimate_scepter"},
+	npc_dota_hero_juggernaut = {tWantedFunItems = {"item_fun_genji_gloves","item_fun_genji_gloves_2","item_fun_ragnarok","item_fun_ragnarok_2","item_fun_blood_sword","item_fun_economizer","item_fun_economizer_2"}, sEndItem = "item_satanic"},
+	npc_dota_hero_nevermore = {tWantedFunItems = {"item_fun_genji_gloves","item_fun_genji_gloves_2","item_fun_blood_sword","item_fun_heros_bow"}, sEndItem = "item_butterfly"},
+	npc_dota_hero_pudge = {tWantedFunItems = {"item_fun_ragnarok","item_fun_ragnarok_2","item_fun_economizer","item_fun_economizer_2","item_fun_magic_hammer","item_fun_escutcheon"}, sEndItem = "item_ultimate_scepter"},
+--	npc_dota_hero_razor = {tWantedFunItems = {"item_fun_ragnarok","item_fun_ragnarok_2","item_fun_genji_gloves","item_fun_genji_gloves_2","item_fun_heros_bow"}, sEndItem = "item_satanic"},
+	npc_dota_hero_sand_king = {tWantedFunItems = {"item_fun_magic_hammer","item_fun_economizer","item_fun_economizer_2","item_fun_escutcheon"}, sEndItem = "item_shivas_guard"},
+	npc_dota_hero_sven = {tWantedFunItems = {"item_fun_papyrus_scarab", "item_fun_blood_sword","item_fun_ragnarok","item_fun_ragnarok_2"}, sEndItem = "item_satanic"},
+	npc_dota_hero_tiny = {tWantedFunItems = {"item_fun_papyrus_scarab","item_fun_ragnarok","item_fun_ragnarok_2","item_fun_blood_sword"}, sEndItem = "item_heart"},
+	npc_dota_hero_vengefulspirit = {tWantedFunItems = {"item_fun_papyrus_scarab","item_fun_genji_gloves","item_fun_genji_gloves_2", "item_fun_magic_hammer"}, sEndItem = "item_aether_lens"},
+	npc_dota_hero_windrunner = {tWantedFunItems = {"item_fun_papyrus_scarab","item_fun_economizer","item_fun_economizer_2","item_fun_genji_gloves","item_fun_genji_gloves_2","item_fun_heros_bow"}, sEndItem = "item_hurricane_pike"},
+	npc_dota_hero_zuus = {tWantedFunItems = {"item_fun_economizer","item_fun_economizer_2","item_fun_magic_hammer","item_fun_escutcheon","item_fun_bs"}, sEndItem = "item_sheepstick"},
+	npc_dota_hero_kunkka = {tWantedFunItems = {"item_fun_blood_sword","item_fun_ragnarok","item_fun_ragnarok_2","item_fun_genji_gloves","item_fun_genji_gloves_2"}, sEndItem = "item_assault"},
+	npc_dota_hero_lina = {tWantedFunItems = {"item_fun_economizer","item_fun_economizer_2","item_fun_heros_bow","item_fun_escutcheon"}, sEndItem = "item_cyclone"},
+	npc_dota_hero_lion = {tWantedFunItems = {"item_fun_papyrus_scarab","item_fun_economizer","item_fun_economizer_2","item_fun_magic_hammer","item_fun_bs"}, sEndItem = "item_dagon_5"},
+	npc_dota_hero_lich = {tWantedFunItems = {"item_fun_economizer","item_fun_economizer_2","item_fun_magic_hammer","item_fun_escutcheon"}, sEndItem = "item_ultimate_scepter"},
+--	npc_dota_hero_tidehunter = {tWantedFunItems = {"item_fun_economizer","item_fun_economizer_2","item_fun_magic_hammer","item_fun_escutcheon"}, sEndItem = "item_shivas_guard"},
+	npc_dota_hero_witch_doctor = {tWantedFunItems = {"item_fun_papyrus_scarab", "item_fun_economizer","item_fun_economizer_2","item_fun_escutcheon"}, sEndItem = "item_black_king_bar"},
+	npc_dota_hero_sniper = {tWantedFunItems = {"item_fun_blood_sword","item_fun_genji_gloves","item_fun_genji_gloves_2","item_fun_heros_bow"}, sEndItem = "item_greater_crit"},
+	npc_dota_hero_necrolyte = {tWantedFunItems = {"item_fun_papyrus_scarab","item_fun_economizer","item_fun_economizer_2","item_fun_magic_hammer"}, sEndItem = "item_octarine_core"},
+	npc_dota_hero_warlock = {tWantedFunItems = {"item_fun_economizer","item_fun_economizer_2","item_fun_magic_hammer","item_fun_escutcheon"}, sEndItem = "item_shivas_guard"},
+	npc_dota_hero_skeleton_king  = {tWantedFunItems = {"item_fun_papyrus_scarab","item_fun_blood_sword","item_fun_genji_gloves","item_fun_genji_gloves_2"}, sEndItem = "item_assault"},
+	npc_dota_hero_death_prophet = {tWantedFunItems = {"item_fun_economizer","item_fun_economizer_2","item_fun_magic_hammer","item_fun_escutcheon"}, sEndItem = "item_shivas_guard"},
+	npc_dota_hero_phantom_assassin = {tWantedFunItems = {"item_fun_blood_sword", 'item_fun_papyrus_scarab',"item_fun_genji_gloves","item_fun_genji_gloves_2"}, sEndItem = "item_monkey_king_bar"},
+	npc_dota_hero_viper = {tWantedFunItems = {"item_fun_papyrus_scarab","item_fun_heros_bow","item_fun_economizer","item_fun_economizer_2","item_fun_genji_gloves","item_fun_genji_gloves_2"}, sEndItem = "item_monkey_king_bar"},
+	npc_dota_hero_luna = {tWantedFunItems = { "item_fun_papyrus_scarab", "item_fun_blood_sword","item_fun_genji_gloves","item_fun_genji_gloves_2"}, sEndItem = "item_assault"},
+	npc_dota_hero_dragon_knight = {tWantedFunItems = {"item_fun_papyrus_scarab","item_fun_blood_sword","item_fun_economizer","item_fun_economizer_2"}, sEndItem = "item_greater_crit"},
+	npc_dota_hero_omniknight = {tWantedFunItems = {"item_fun_papyrus_scarab","item_fun_economizer","item_fun_economizer_2","item_fun_ragnarok","item_fun_ragnarok_2", "item_fun_escutcheon"}, sEndItem = "item_heart"},
+	npc_dota_hero_jakiro = {tWantedFunItems = {"item_fun_papyrus_scarab","item_fun_economizer","item_fun_economizer_2","item_fun_magic_hammer"}, sEndItem = "item_ultimate_scepter"},
+	npc_dota_hero_chaos_knight = {tWantedFunItems = {"item_fun_ragnarok","item_fun_ragnarok_2","item_fun_genji_gloves","item_fun_genji_gloves_2","item_fun_papyrus_scarab", "item_fun_economizer"}, sEndItem = "item_assault"},
+	npc_dota_hero_bristleback = {tWantedFunItems = {"item_fun_papyrus_scarab", "item_fun_ragnarok","item_fun_ragnarok_2", "item_fun_economizer", "item_fun_economizer_2","item_fun_magic_hammer"}, sEndItem = "item_radiance"},
+	npc_dota_hero_skywrath_mage = {tWantedFunItems = {"item_fun_economizer","item_fun_economizer_2","item_fun_magic_hammer","item_fun_escutcheon"}, sEndItem = "item_sheepstick"},
+	npc_dota_hero_oracle = {tWantedFunItems = {"item_fun_papyrus_scarab","item_fun_economizer","item_fun_economizer_2","item_fun_magic_hammer"}, sEndItem = "item_orchid"},
+}
+
+--update end item before fun item
+for k, v in pairs(tBotItemData.tLuxuryItemList) do
+	tBotItemData.tBotBuildFunItems[k].sEndItem = v[#v]
+end
+
+tBotItemData.tWrongItems = {
+	{tRightItems = {'item_butterfly', 'item_satanic'}, components = {item_mask_of_madness = 1,item_eagle = 1, item_reaver =1, item_talisman_of_evasion = 1, item_claymore= 1}, deficit = 0},
+	{tRightItems = {'item_black_king_bar'}, components = {item_javelin = 1, item_ogre_axe = 1, item_recipe_black_king_bar = 1}, deficit = 500},
+	{tRightItems = {'item_black_king_bar', 'item_oblivion_staff'}, components = {item_mithril_hammer = 1, item_echo_sabre = 1, item_recipe_black_king_bar = 1}, deficit = 0},
+}
+tBotItemData.tLowCostItems = {
+	item_flask = true;
+	item_branches = true;
+	item_bottle = true;
+	item_clarity = true;
+	item_tango = true;
+	item_magic_stick = true;
+	item_magic_wand = true;
+	item_recipe_magic_wand = true;
+	item_mantle = true;
+	item_wraith_band = true;
+	item_null_talisman = true;
+	item_bracer = true;
+--	item_ring_of_basilius = true;
+--	item_stout_shield = true;
+--	item_quelling_blade = true;
+}
